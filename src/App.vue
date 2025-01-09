1acb12736b5ad023e25c167e973c9d697fa2fbd1
UWASE NDEGEYA Nina <uwasendegeya@gmail.com>
2025-01-09 07:11:34 +0200

    incr/decr in vue.js

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+<div><h1>incrementation and decrementation</h1></div>
+  <div>
+    <button @click="handleIncrement">increment</button>
+    <button>{{ count }}</button>
+    <button @click="handleDecrement">decrement</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      count: 0, 
+    };
+  },
+  methods: {
+    handleIncrement() {
+      this.count++; 
+    },
+    handleDecrement() {
+      this.count--; 
+    },
+  },
+};
+</script>
+
+<style>
+button {
+  font-family: Arial, sans-serif;
+  color: blue;
+  width: 15rem;
+  height: 0%;
+  margin-left: 450px;
+  background-color: blue;
+  color:white;
+}
+h1{
+  margin-left:350px;
+}
+</style>
